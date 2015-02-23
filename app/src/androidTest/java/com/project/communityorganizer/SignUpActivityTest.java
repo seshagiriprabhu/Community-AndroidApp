c7cd15e2726338c165672716574a5a407b64604a
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2015-02-23 20:27:55 +0100

    Adding test suite for signup activity

---
+++ app/src/androidTest/java/com/project/communityorganizer/SignUpActivityTest.java
@@ -0,0 +1,18 @@
+package com.project.communityorganizer;
+
+import junit.framework.TestCase;
+
+public class SignUpActivityTest extends TestCase {
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnClick() throws Exception {
+
+    }
+
+    public void testIsConnected() throws Exception {
+
+    }
+}
