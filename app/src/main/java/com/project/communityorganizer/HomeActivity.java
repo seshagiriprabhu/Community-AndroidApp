b37bb737329a6753de3dc20ee980ee87527392c2
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2015-02-24 10:00:03 +0100

    Adding homeactivity base structure[incomplete]

--- app/src/main/java/com/project/communityorganizer/HomeActivity.java
+++ app/src/main/java/com/project/communityorganizer/HomeActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class HomeActivity extends ActionBarActivity {
@@ -12,6 +18,17 @@ public class HomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                new String[] {"Event", "Friend", "Location", "Settings"}));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
